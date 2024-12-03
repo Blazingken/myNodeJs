67e9317e95095e59ae49141845ad57e92c31dfb1
Yann Kenzo <kunjemkenzo500@gmail.com>
2024-12-03 16:32:05 +0100

    version 4

--- demo_module.js
+++ demo_module.js
@@ -6,3 +6,11 @@ http.createServer(function (req, res)  {
        res.write(`The date and time are currently: ${dt.myDateTime()}`);
        res.end();
 }).listen(8080); */
+
+const {readFileSync, writeFileSync} = require('fs')
+
+const first = readFileSync('./myfile.txt', 'utf8')
+const second = readFileSync('./myfile2.txt', 'utf8')
+
+console.log(first)
+console.log(second)
